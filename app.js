94f4bef8d5258f99b2510fea8936f358b4c0d9c6
KickButtowski80 <pazpaz22@yahoo.com>
2023-01-15 21:07:00 -0800

    set up app.js

--- app.js
+++ app.js
@@ -1,2 +1,34 @@
 require('dotenv').config()
 // async errors
+
+const express = require('express')
+const app = express()
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
+})
+
+
+// products route
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 3000
+
+const start = async () => {
+    try {
+        //connect DB
+
+        app.listen(port, console.log(`Server is listening 👓 port ${port}... 🚀`))
+    } catch (error) {
+        console.log(`error🚑 ${error}`)
+    }
+}
+
+start()
