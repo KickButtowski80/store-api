78903bd25b51e5d38cc868d4fb4a9866fe5e551c
KickButtowski80 <pazpaz22@yahoo.com>
2023-01-15 21:47:29 -0800

    connected to mongoose atlas

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const connectDB = require('./db/connect')
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
 
@@ -13,7 +14,7 @@ app.get('/', (req, res) => {
     res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
 })
 
-
+ 
 // products route
 
 app.use(notFoundMiddleware)
@@ -24,7 +25,7 @@ const port = process.env.PORT || 3000
 const start = async () => {
     try {
         //connect DB
-
+        await connectDB(process.env.MONGO_URI)  
         app.listen(port, console.log(`Server is listening 👓 port ${port}... 🚀`))
     } catch (error) {
         console.log(`error🚑 ${error}`)
