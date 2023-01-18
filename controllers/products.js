33ad31a5e44e1aa4b629d54841b164981b293b3e
KickButtowski80 <pazpaz22@yahoo.com>
2023-01-18 13:47:47 -0800

    add the sort functionality

--- controllers/products.js
+++ controllers/products.js
@@ -1,18 +1,19 @@
 const Product = require('../models/product')
-
+ 
 
 
 const getAllProductsStatic = async (req, res) => {
     const search = 'a'
-    const products = await Product.find({
-        featured: true,
-        name: { $regex: search, $options: 'i' }
-    })
+    // const products = await Product.find({
+    //     featured: true,
+    //     name: { $regex: search, $options: 'i' }
+    // })
+    const products = await Product.find({}).sort('-name price')
     res.status(200).json({ count: products.length, products: products })
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name, } = req.query
+    const { featured, company, name, sort } = req.query
     const queryObject = {}
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false
@@ -23,8 +24,17 @@ const getAllProducts = async (req, res) => {
     if (name) {
         queryObject.name = { $regex: name, $options: 'i' }
     }
+    
+    let result = Product.find(queryObject)
 
-    const products = await Product.find(queryObject)
+    if (sort) {
+      const sortList = sort.split(',').join(' ')
+      result = result.sort(sortList)
+    } else {
+        result = result.sort('createdAt')
+    }
+
+    const products = await result;
     res.status(200).json({ count: products.length, products: products })
 
 }
