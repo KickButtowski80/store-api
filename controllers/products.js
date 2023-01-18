dd93350e73947b0a52c223c2987ece28ad484a5e
KickButtowski80 <pazpaz22@yahoo.com>
2023-01-17 20:09:40 -0800

    implemented static search products

--- controllers/products.js
+++ controllers/products.js
@@ -1,6 +1,13 @@
-const getAllProductsStatic = async (req, res) => {
-    throw new Error('testing aysnc errors 🚑')
-    res.status(200).json({ msg: 'products testing route 🚗' })
+const Product = require('../models/product')
+
+
+
+const getAllProductsStatic = async (req, res) => {  
+    const products = await Product.find({
+         featured: true,
+         name: 'vase table'
+         })
+    res.status(200).json({ count: products.length, products: products })
 }
 
 const getAllProducts = async (req, res) => {
