d46b1a45e9b6ef19e6c8c65995c45e812c7f83e4
KickButtowski80 <pazpaz22@yahoo.com>
2023-01-18 19:29:34 -0800

    implemented skip and limit

--- controllers/products.js
+++ controllers/products.js
@@ -8,12 +8,18 @@ const getAllProductsStatic = async (req, res) => {
     //     featured: true,
     //     name: { $regex: search, $options: 'i' }
     // })
-    const products = await Product.find({}).sort('-name price')
+    // const products = await Product.find({}).sort('-name price')
+    const products = await Product.find({})
+    .sort('name')
+    .select('name price')
+    .skip(5)
+    .limit(5)
+  
     res.status(200).json({ count: products.length, products: products })
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name, sort } = req.query
+    const { featured, company, name, sort, fields } = req.query
     const queryObject = {}
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false
@@ -33,6 +39,17 @@ const getAllProducts = async (req, res) => {
     } else {
         result = result.sort('createdAt')
     }
+
+    if (fields) {
+        const feildList = fields.split(',').join(' ')
+        result = result.select(feildList)
+    }
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+
+    result = result.skip(skip).limit(limit)
 
     const products = await result;
     res.status(200).json({ count: products.length, products: products })
